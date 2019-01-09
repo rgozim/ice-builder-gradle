c5383a45a171fdc3eb22ab99c81394620b862708
Riad Gozim <rgozim1@gmail.com>
2019-01-09 11:19:18 +0000

    Fixed file deletion in incremental task

--- src/main/groovy/com/zeroc/gradle/icebuilder/slice/PythonTask.groovy
+++ src/main/groovy/com/zeroc/gradle/icebuilder/slice/PythonTask.groovy
@@ -1,5 +1,6 @@
 package com.zeroc.gradle.icebuilder.slice
 
+import org.apache.commons.io.FilenameUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.file.FileCollection
@@ -11,7 +12,8 @@ import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.incremental.IncrementalTaskInputs
-import org.gradle.internal.impldep.org.apache.commons.io.FilenameUtils
+
+import static groovy.io.FileType.FILES
 
 class PythonTask extends DefaultTask {
 
@@ -36,39 +38,63 @@ class PythonTask extends DefaultTask {
 
     @TaskAction
     void action(IncrementalTaskInputs inputs) {
-        if (!inputs.incremental)
-            project.delete(outputDir.listFiles())
-
-        List<File> files = []
-        inputs.outOfDate { change ->
-            if (change.file.isFile()) {
-                Log.info("$change.file")
-                files.add(change.file)
+        if (!inputs.incremental) {
+            inputDir.traverse(type: FILES) { file ->
+                deleteOutputFile(file)
             }
         }
 
-        if (!files.isEmpty()) {
-            List cmd = [sliceExt.slice2py, "-I${sliceExt.sliceDir}", "-I${inputDir}"]
-            files.each { file ->
-                cmd.add(file.getAbsoluteFile())
+        List filesForProcessing = []
+        inputs.outOfDate { change ->
+            if (change.file.directory) return
+
+            // Add input file for processing
+            filesForProcessing.add("${change.file}")
+        }
+
+        if (!filesForProcessing.isEmpty()) {
+            List cmd = [sliceExt.slice2py, "-I${sliceExt.sliceDir}"]
+
+            if (includeDirs) {
+                // Add any additional include dirs
+                includeDirs.each { dir -> cmd.add("-I${dir}") }
             }
+
+            // Add files for processing
+            cmd.addAll(filesForProcessing)
+
             if (prefix) {
+                // Set a prefix
                 cmd.add("--prefix=${prefix}")
             }
+
+            // Set the output directory
             cmd.add("--output-dir=${outputDir}")
             executeCommand(cmd)
         }
 
-        inputs.removed { removed ->
-            def extension = FilenameUtils.getExtension(removed.file.name)
-            def filename = FilenameUtils.getBaseName(removed.file.name)
-            if (prefix) {
-                filename = prefix + filename + "_ice"
-            }
-            def targetFile = project.file("$outputDir/${filename}.{$extension}")
-            if (targetFile.exists()) {
-                targetFile.delete()
-            }
+        inputs.removed { change ->
+            if (change.file.directory) return
+
+            deleteOutputFile(change.file)
+        }
+    }
+
+    String getOutputFileName(File file) {
+        def extension = FilenameUtils.getExtension(file.name)
+        def filename = FilenameUtils.getBaseName(file.name)
+        if (prefix) {
+            filename = prefix + filename + "_ice"
+        }
+        return "${filename}.{$extension}"
+    }
+
+    void deleteOutputFile(File file) {
+        // Convert the input filename to the output filename and
+        // delete that file
+        def targetFile = project.file("$outputDir/${getOutputFileName(file)}")
+        if (targetFile.exists()) {
+            targetFile.delete()
         }
     }
 
