629da1d04acb0f40ca406f6072c4000d5b2e677e
Riad Gozim <rgozim1@gmail.com>
2019-01-23 17:56:46 +0000

    Up to date changes

--- src/main/groovy/com/zeroc/gradle/icebuilder/slice/tasks/DependencyTask.groovy
+++ src/main/groovy/com/zeroc/gradle/icebuilder/slice/tasks/DependencyTask.groovy
@@ -5,10 +5,7 @@ import groovy.transform.Internal
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.file.ConfigurableFileCollection
-import org.gradle.api.file.RegularFile
 import org.gradle.api.logging.Logging
-import org.gradle.api.provider.ListProperty
-import org.gradle.api.provider.MapProperty
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Optional
@@ -20,18 +17,16 @@ class DependencyTask extends DefaultTask {
     private static final def Log = Logging.getLogger(DependencyTask)
 
     @InputFiles
-    final ListProperty<RegularFile> inputFiles = project.objects.listProperty(RegularFile)
+    final ConfigurableFileCollection inputFiles = project.layout.configurableFiles()
 
     @Optional
     @Input
     final ConfigurableFileCollection includeDirs = project.layout.configurableFiles()
 
-    // @OutputFiles
-    // final ConfigurableFileCollection outputFiles = project.layout.configurableFiles()
+    @Internal
+    Set results
 
     @Internal
-    final MapProperty<File, List> results = project.objects.mapProperty(File, List)
-
     private Configuration config = new Configuration()
 
     @TaskAction
@@ -51,15 +46,10 @@ class DependencyTask extends DefaultTask {
 
         def fileSet = parseSliceDependencyXML(sout)
 
-        // Set result
-        results.set(fileSet)
-        // outputFiles.setFrom(fileSet)
+        // These files are dependencies of the input ice files
+        results = fileSet
     }
 
-//    FileCollection getResult() {
-//        return result
-//    }
-//
 //    void includeDirs(FileCollection collection) {
 //        setIncludeDirs(collection)
 //    }
@@ -89,56 +79,55 @@ class DependencyTask extends DefaultTask {
     //   <source name="Hello.ice">
     //   </source>
     // </dependencies>
-    Map<File, List<File>> parseSliceDependencyXML(xml) {
+    Set<Dependency> parseSliceDependencyXML(StringBuffer sout) {
+        def xml = new XmlSlurper().parseText(sout.toString())
         if (xml.name() != "dependencies") {
             throw new GradleException("malformed XML")
         }
 
-        Map<File, List<File>> dependencies = [:]
+        Set<Dependency> dependencies = []
         xml.children().each {
             if (it.name() == "source") {
-                def source = it.attributes().get("name")
-                def files = []
+                File source = new File(it.attributes().get("name"))
                 it.children().each {
                     if (it.name() == "dependsOn") {
-                        def dependsOn = new File(it.attributes().get("name"))
-                        files.add(dependsOn)
+                        File dependsOn = new File(it.attributes().get("name"))
+
+                        def dependency = dependencies.find {
+                            it.self == dependsOn
+                        }
+
+                        if (dependency) {
+                            dependency.sources.add(source)
+                        } else {
+                            List sourceList = [source]
+                            dependencies.add(new Dependency(sourceList, dependsOn))
+                        }
                     }
                 }
-                dependencies.put(new File(source), files)
             }
         }
+
         return dependencies
     }
 
+    static class Dependency {
+        List sources = []
 
-    // Parse the dependency XML which is of the format:
-    //
-    // <dependencies>
-    //   <source name="A.ice">
-    //     <dependsOn name="Hello.ice"/>
-    //   </source>
-    //   <source name="Hello.ice">
-    //   </source>
-    // </dependencies>
-//    static Set<File> parseSliceDependencyXML(StringBuffer sout) {
-//        def xml = new XmlSlurper().parseText(sout.toString())
-//        if (xml.name() != "dependencies") {
-//            throw new GradleException("malformed XML")
-//        }
-//
-//        Set dependencies = []
-//        xml.children().each {
-//            if (it.name() == "source") {
-//                it.children().each {
-//                    if (it.name() == "dependsOn") {
-//                        def dependsOn = new File(it.attributes().get("name"))
-//                        dependencies.add(dependsOn)
-//                    }
-//                }
-//            }
-//        }
-//
-//        return dependencies
-//    }
+        File self
+
+        Dependency(List sources, File self) {
+            this.sources = sources
+            this.self = self
+        }
+
+        @Override
+        String toString() {
+            def s = "Sources with dependency $self : \n"
+            sources.each {
+                s = s + "$it\n"
+            }
+            return s
+        }
+    }
 }
