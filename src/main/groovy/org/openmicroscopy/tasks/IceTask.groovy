d979df7f8e6f38f76b65de23dc71a3090b046144
Riad Gozim <rgozim1@gmail.com>
2019-06-05 12:19:32 +0100

    Added base class for ice tasks

---
+++ src/main/groovy/org/openmicroscopy/tasks/IceTask.groovy
@@ -0,0 +1,77 @@
+package org.openmicroscopy.tasks
+
+import com.zeroc.gradle.icebuilder.slice.SliceExtension
+import org.gradle.api.GradleException
+import org.gradle.api.file.Directory
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.SourceTask
+
+abstract class IceTask extends SourceTask {
+
+    @Input
+    @Optional
+    final Property<Boolean> debug = project.objects.property(Boolean)
+
+    @InputFiles
+    @Optional
+    final ListProperty<Directory> includeDirs = project.objects.listProperty(Directory)
+
+    @OutputDirectory
+    final DirectoryProperty outputDir = project.objects.directoryProperty()
+
+    // Change this to a configuration
+    SliceExtension sliceExt = project.slice
+
+    private final String iceCommand
+
+    private IceTask() {
+        super()
+    }
+
+    IceTask(String iceCommand) {
+        super()
+        this.iceCommand = iceCommand
+        this.setIncludes(["**/*.ice"])
+    }
+
+    List<String> createBaseCompileSpec() {
+        List<String> cmd = [iceCommand]
+
+        if (debug.getOrElse(false)) {
+            cmd.add("-d")
+        }
+
+        // Set the output directory
+        cmd.addAll(["--output-dir", String.valueOf(outputDir.asFile.get())])
+
+        // Add internal slice dir as default include
+        cmd.add("-I" + sliceExt.sliceDir)
+
+        // Include directories used for "include/import" directives in ice files
+        List<Directory> includeDirsList = includeDirs.getOrNull()
+        if (includeDirsList) {
+            // Add any additional includes
+            includeDirsList.each { Directory dir ->
+                cmd.add("-I" + dir.asFile)
+            }
+        }
+
+        return cmd
+    }
+
+    void executeCommand(List<String> cmd) {
+        StringBuffer sout = new StringBuffer()
+        Process p = cmd.execute()
+        p.waitForProcessOutput(sout, System.err)
+        if (p.exitValue() != 0) {
+            throw new GradleException("${cmd[0]} failed with exit code: ${p.exitValue()}")
+        }
+    }
+
+}
