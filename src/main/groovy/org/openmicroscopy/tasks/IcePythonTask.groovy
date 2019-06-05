6da6bcf57e8f2cfff837106754ffe144a64b8f5e
Riad Gozim <rgozim1@gmail.com>
2019-06-05 12:19:32 +0100

    Added base class for ice tasks

--- src/main/groovy/org/openmicroscopy/tasks/IcePythonTask.groovy
+++ src/main/groovy/org/openmicroscopy/tasks/IcePythonTask.groovy
@@ -1,67 +1,43 @@
 package org.openmicroscopy.tasks
 
-import com.zeroc.gradle.icebuilder.slice.SliceExtension
+
 import org.apache.commons.io.FilenameUtils
-import org.gradle.api.DefaultTask
-import org.gradle.api.GradleException
-import org.gradle.api.file.ConfigurableFileCollection
-import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.logging.Logging
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Optional
-import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.incremental.IncrementalTaskInputs
+import org.gradle.api.tasks.incremental.InputFileDetails
 
-class IcePythonTask extends DefaultTask {
-
-    private static final def Log = Logging.getLogger(IcePythonTask)
-
-    @InputFiles
-    final ConfigurableFileCollection sourceFiles = project.files()
-
-    @InputFiles
-    @Optional
-    final ConfigurableFileCollection includeDirs = project.files()
+class IcePythonTask extends IceTask {
 
     @Input
     @Optional
     final Property<String> prefix = project.objects.property(String)
 
-    @OutputDirectory
-    final DirectoryProperty outputDir = project.objects.directoryProperty()
-
-    // Change this to a configuration
-    SliceExtension sliceExt = project.slice
+    IcePythonTask(String iceCommand) {
+        super("slice2py")
+    }
 
     @TaskAction
     void action(IncrementalTaskInputs inputs) {
+        // If the user for example used --rerun-tasks
+        // this task is not incremental. Only
+        // inputs.outOfDate is executed, so we must first
+        // remove all output files.
         if (!inputs.incremental) {
-            sourceFiles.files.each { file ->
-                deleteOutputFile(file)
-            }
+            project.delete(outputDir.asFile.get().listFiles())
         }
 
+        // Input file has changed, so we convert it.
         List<String> filesForProcessing = []
-        inputs.outOfDate { change ->
-            if (change.file.directory) return
-
-            // Log which file will be included in slice2py
-            Log.info("File for processing: $change.file")
-
+        inputs.outOfDate { InputFileDetails outOfDate ->
             // Add input file for processing
-            filesForProcessing.add(String.valueOf(change.file))
+            filesForProcessing.add(String.valueOf(outOfDate.file))
         }
 
         if (!filesForProcessing.isEmpty()) {
-            List<String> cmd = ["slice2py", "-I" + sliceExt.sliceDir]
-
-            if (!includeDirs.isEmpty()) {
-                // Add any additional includes
-                includeDirs.files.each { dir -> cmd.add("-I" + dir) }
-            }
+            List<String> cmd = createBaseCompileSpec()
 
             // Add files for processing
             cmd.addAll(filesForProcessing)
@@ -71,14 +47,10 @@ class IcePythonTask extends DefaultTask {
                 cmd.add("--prefix=" + prefix.get())
             }
 
-            // Set the output directory
-            cmd.add("--output-dir=" + outputDir.asFile.get())
             executeCommand(cmd)
         }
 
         inputs.removed { change ->
-            if (change.file.directory) return
-
             deleteOutputFile(change.file)
         }
     }
@@ -126,12 +98,4 @@ class IcePythonTask extends DefaultTask {
         return "${filename}.{$extension}"
     }
 
-    void executeCommand(List cmd) {
-        StringBuffer sout = new StringBuffer()
-        Process p = cmd.execute()
-        p.waitForProcessOutput(sout, System.err)
-        if (p.exitValue() != 0) {
-            throw new GradleException("${cmd[0]} failed with exit code: ${p.exitValue()}")
-        }
-    }
 }
